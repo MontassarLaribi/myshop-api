6bae5eb461d1c32fa2f24118b7d2889b70c544f4
Montassar Laribi <mohamedmontassar.laribi@esprit.tn>
2021-01-25 21:24:36 +0100

    App config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,35 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AppController } from './app.controller';
+import { CacheInterceptor, CacheModule, Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { join } from 'path';
+import { ConfigModule } from '@nestjs/config';
+// MODULES
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    CacheModule.register(),
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      host: process.env.DB_HOST,
+      database: process.env.DB_NAME,
+      entities: [join(__dirname, '**/entities/**.entity{.ts,.js}')],
+      synchronize: true,
+      logger: 'advanced-console',
+      useUnifiedTopology: true
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CacheInterceptor
+    },
+    AppService
+  ]
 })
 export class AppModule {}
